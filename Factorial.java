ce11d095c5244ea2bf7f7405c6ad20671ca30f7b
Diksha-Kataria <73074875+Diksha-Kataria@users.noreply.github.com>
2021-09-06 23:57:03 +0530

    To print a factorial.

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Factorial{  
+ public static void main(String args[]){  
+  int i,fact=1; 
+System.out.println("Enter the number: ");
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();    
+  for(i=1;i<=n;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+n+" is: "+fact);    
+ }  
+}  
