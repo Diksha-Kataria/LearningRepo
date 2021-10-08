e48bcfbfe7ab81903b24757a1abb08abb7997e13
Diksha-Kataria <73074875+Diksha-Kataria@users.noreply.github.com>
2021-10-09 04:56:46 +0530

    Printing a diamond pattern with *.

---
+++ Pattern14.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Pattern14
+{
+public static void main(String[] args)
+{
+int n,i, j, k;
+System.out.print("Enter the number: ");
+Scanner scanner = new Scanner(System.in);
+n = scanner.nextInt();
+
+for(i = 1; i <= n; i++)
+{
+for(j = n-1; j >= i; j--)
+{
+System.out.print(" ");
+}
+for(k = 1; k <= i; k++)
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+for(i = 1; i <= n; i++)
+{
+for(j = 1; j<= i; j++)
+{
+System.out.print(" ");
+}
+for(k = n-1; k >= i; k--)
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}
